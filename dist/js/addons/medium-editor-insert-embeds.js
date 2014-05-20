c3e76e14f7d7b2fa260ea157de4444ebd52a3114
tooyama <rinrin900@gmail.com>
2014-05-20 16:36:40 +0900

    modified embed styles

--- dist/js/addons/medium-editor-insert-embeds.js
+++ dist/js/addons/medium-editor-insert-embeds.js
@@ -36,33 +36,37 @@
      * @return {void}
      */
     add : function ($placeholder) {
-      $(".mediumInsert-embedsPlaceholder").remove();
       $.fn.mediumInsert.insert.deselect();
-      $placeholder.append('<div class="mediumInsert-embedsPlaceholder">' +
-           '<i class="fa fa-code mediumInsert-embedsImage"></i>' +
-           '<div class="mediumInsert-embedsWire">' +
-             '<div class="mediumInsert-embedsBox">' +
-               '<input type="text" class="mediumInsert-embedsText" placeholder="type or paste url here">' +
-               '<button class="mediumInsert-embedsButton">Embed</button>' +
-             '</div>' +
-           '</div>' +
-         '</div>');
+
+
+      var formHtml = '<div class="medium-editor-toolbar-form-anchor mediumInsert-embedsWire" style="display: block;"><input type="text" value="" placeholder="type or paste url here" class="mediumInsert-embedsText"></div>';
+      $(formHtml).appendTo($placeholder.prev());
+      setTimeout(function () {
+        $placeholder.prev().find('input').focus();
+      }, 50);
+
+      $.fn.mediumInsert.insert.deselect();
+
       this.currentPlaceholder = $placeholder;
       $(".mediumInsert-embedsText").focus();
     },
 
+
     setEmbedButtonEvents : function () {
       var that = this;
-      $(document).on('click', 'button.mediumInsert-embedsButton', function () {
-        that.setEnterActionEvents();
-      });
       $(document).on('keypress', 'input.mediumInsert-embedsText', function (e) {
         if ((e.which && e.which == 13) || (e.keyCode && e.keyCode == 13)) {
           that.setEnterActionEvents();
+          that.removeToolbar();
         }
       });
+
+      this.$el.on('blur', '.mediumInsert-embedsText', function () {
+        that.removeToolbar();
+      });
+
     },
-    setEnterActionEvents : function() {
+    setEnterActionEvents : function () {
       var that = this;
       if ($.fn.mediumInsert.settings.enabled === false) {
         return;
@@ -79,13 +83,16 @@
         embed_tag = $('<div class="mediumInsert-embeds"></div>').append(embed_tag);
         that.currentPlaceholder.parent().append(embed_tag);
       }
-      $(".mediumInsert-embedsPlaceholder").remove();
+    },
+
+    removeToolbar : function () {
+      $(".mediumInsert-embedsWire").remove();
     },
 
     convertUrlToEmbedTag : function (url) {
-      var embed_tag = url.replace(/\n?/g, '').replace(/https?:\/\/www\.youtube\.com\/watch\?v=(.+?)$/, '<div class="video"><iframe width="420" height="315" src="//www.youtube.com/embed/$1" frameborder="0" allowfullscreen></iframe></div>')
+      var embed_tag = url.replace(/\n?/g, '').replace(/^((http(s)?:\/\/)?(www\.)?(youtube\.com|youtu\.be)\/(watch\?v=|v\/)?)([a-zA-Z0-9-]+)(.*)?$/, '<div class="video"><iframe width="420" height="315" src="http://www.youtube.com/v/$7&amp;fs=1" frameborder="0" allowfullscreen></iframe></div>')
         .replace(/http:\/\/vimeo\.com\/(\d+)$/, '<iframe src="//player.vimeo.com/video/$1" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>')
-        .replace(/https:\/\/twitter\.com\/(\w+)\/status\/(\d+)\/?$/, '<blockquote class="twitter-tweet" lang="ja"><a href="https://twitter.com/$1/statuses/$2"></a></blockquote><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>')
+        .replace(/https:\/\/twitter\.com\/(\w+)\/status\/(\d+)\/?$/, '<blockquote class="twitter-tweet" lang="en"><a href="https://twitter.com/$1/statuses/$2"></a></blockquote><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>')
         .replace(/https:\/\/www\.facebook\.com\/(\w+)\/posts\/(\d+)$/, '<div id="fb-root"></div><script>(function(d, s, id) { var js, fjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) return; js = d.createElement(s); js.id = id; js.src = "//connect.facebook.net/en_US/all.js#xfbml=1"; fjs.parentNode.insertBefore(js, fjs); }(document, "script", "facebook-jssdk"));</script><div class="fb-post" data-href="https://www.facebook.com/$1/posts/$2"></div>')
         .replace(/http:\/\/instagram\.com\/p\/(.+)\/?$/, '<span class="instagram"><iframe src="//instagram.com/p/$1/embed/" width="612" height="710" frameborder="0" scrolling="no" allowtransparency="true"></iframe></span>');
       return /<("[^"]*"|'[^']*'|[^'">])*>/.test(embed_tag) ? embed_tag : false;
--- test/medium-editor-insert-embeds.js
+++ test/medium-editor-insert-embeds.js
@@ -1,13 +1,13 @@
 /**
-* Images
-*/
+ * Images
+ */
 
 module("embeds", {
-  setup: function() {
+  setup : function () {
     $.fn.mediumInsert.settings.editor = new MediumEditor('.editable');
     $.fn.mediumInsert.settings.addons = {
-      images: {
-        imagesUploadScript: 'examples/upload.php'
+      images : {
+        imagesUploadScript : 'examples/upload.php'
       }
     };
     $.fn.mediumInsert.settings.enabled = true;
@@ -15,7 +15,6 @@ module("embeds", {
     $.fn.mediumInsert.insert.$el = $('#qunit-fixture');
   }
 });
-
 
 // init
 
@@ -28,171 +27,22 @@ test('init() sets addon\'s $el', function () {
 });
 
 
-
-// add
-
-test('add() adds embed block forms', function () {
-  var $el = $('#qunit-fixture').html('<div class="mediumInsert-placeholder"></div>'),
-      $placeholder =  $('.mediumInsert-placeholder', $el);
-
-  $.fn.mediumInsert.getAddon('embeds').add($placeholder);
-  equal($placeholder.html(), '<div class="mediumInsert-embedsPlaceholder">' +
-      '<i class="fa fa-code mediumInsert-embedsImage"></i>' +
-      '<div class="mediumInsert-embedsWire">' +
-        '<div class="mediumInsert-embedsBox">' +
-          '<input type="text" class="mediumInsert-embedsText" placeholder="type or paste url here">' +
-          '<button class="mediumInsert-embedsButton">Embed</button>' +
-        '</div>' +
-      '</div>' +
-    '</div>', 'Embed block added');
-});
-
-
-asyncTest('embed "Twitter" url', function () {
-  var $el = $('#qunit-fixture').html('<div class="mediumInsert" contenteditable="false">' +
-    '<div class="mediumInsert-placeholder">' +
-      '<div class="mediumInsert-embedsPlaceholder">' +
-          '<i class="fa fa-code mediumInsert-embedsImage"></i>' +
-          '<div class="mediumInsert-embedsWire">' +
-            '<div class="mediumInsert-embedsBox">' +
-              '<input type="text" class="mediumInsert-embedsText" placeholder="type or paste url here">' +
-              '<button class="mediumInsert-embedsButton">Embed</button>' +
-            '</div>' +
-          '</div>' +
-        '</div>' +
-      '</div>' +
-    '</div>'
-  );
+asyncTest('embed some SNS service url', function () {
+  var $el = $('#qunit-fixture').html('<div class="medium-editor-toolbar-form-anchor mediumInsert-embedsWire" style="display: block;"><input type="text" value="" placeholder="type or paste url here" class="mediumInsert-embedsText"></div>');
 
   $.fn.mediumInsert.getAddon('embeds');
 
-  equal($('.mediumInsert-embedsPlaceholder', $el).length, 1, 'embed placeholder removed');
-  $(document).one('click', '.mediumInsert-embedsButton', function () {
-    equal($('.mediumInsert-embedsPlaceholder', $el).length, 0, 'embed placeholder removed');
-    ok($('.mediumInsert-placeholder', $el).length > 0, 'embed social service tags');
+  $(document).one('keypress', '.mediumInsert-embedsText', function () {
+    equal($('.mediumInsert-embedsWire', $el).length, 0, 'embed wire removed');
     start();
   });
 
-  $('.mediumInsert-embedsText', $el).val('https://twitter.com/usako1225/status/300910402538192897/');
-  $('.mediumInsert-embedsButton', $el).click();
-
-  $('.mediumInsert-embedsText', $el).val('https://www.facebook.com/kensuu/posts/10152132714538568');
-  $('.mediumInsert-embedsButton', $el).click();
-});
-
-asyncTest('embed "Facebook" url', function () {
-  var $el = $('#qunit-fixture').html('<div class="mediumInsert" contenteditable="false">' +
-    '<div class="mediumInsert-placeholder">' +
-      '<div class="mediumInsert-embedsPlaceholder">' +
-          '<i class="fa fa-code mediumInsert-embedsImage"></i>' +
-          '<div class="mediumInsert-embedsWire">' +
-            '<div class="mediumInsert-embedsBox">' +
-              '<input type="text" class="mediumInsert-embedsText" placeholder="type or paste url here">' +
-              '<button class="mediumInsert-embedsButton">Embed</button>' +
-            '</div>' +
-          '</div>' +
-        '</div>' +
-      '</div>' +
-    '</div>'
-  );
-
-  $.fn.mediumInsert.getAddon('embeds');
-
-  equal($('.mediumInsert-embedsPlaceholder', $el).length, 1, 'embed placeholder removed');
-  $(document).one('click', '.mediumInsert-embedsButton', function () {
-    equal($('.mediumInsert-embedsPlaceholder', $el).length, 0, 'embed placeholder removed');
-    ok($('.mediumInsert-placeholder', $el).length > 0, 'embed social service tags');
-    start();
-  });
-
-  $('.mediumInsert-embedsText', $el).val('https://www.facebook.com/kensuu/posts/10152132714538568');
-  $('.mediumInsert-embedsButton', $el).click();
-});
-
-asyncTest('embed "Vimeo" url', function () {
-  var $el = $('#qunit-fixture').html('<div class="mediumInsert" contenteditable="false">' +
-    '<div class="mediumInsert-placeholder">' +
-      '<div class="mediumInsert-embedsPlaceholder">' +
-          '<i class="fa fa-code mediumInsert-embedsImage"></i>' +
-          '<div class="mediumInsert-embedsWire">' +
-            '<div class="mediumInsert-embedsBox">' +
-              '<input type="text" class="mediumInsert-embedsText" placeholder="type or paste url here">' +
-              '<button class="mediumInsert-embedsButton">Embed</button>' +
-            '</div>' +
-          '</div>' +
-        '</div>' +
-      '</div>' +
-    '</div>'
-  );
-
-  $.fn.mediumInsert.getAddon('embeds');
-
-  equal($('.mediumInsert-embedsPlaceholder', $el).length, 1, 'embed placeholder removed');
-  $(document).one('click', '.mediumInsert-embedsButton', function () {
-    equal($('.mediumInsert-embedsPlaceholder', $el).length, 0, 'embed placeholder removed');
-    ok($('.mediumInsert-placeholder', $el).length > 0, 'embed social service tags');
-    start();
-  });
-
-  $('.mediumInsert-embedsText', $el).val('http://vimeo.com/94923911');
-  $('.mediumInsert-embedsButton', $el).click();
-});
-
-asyncTest('embed "Youtube" url', function () {
-  var $el = $('#qunit-fixture').html('<div class="mediumInsert" contenteditable="false">' +
-    '<div class="mediumInsert-placeholder">' +
-      '<div class="mediumInsert-embedsPlaceholder">' +
-          '<i class="fa fa-code mediumInsert-embedsImage"></i>' +
-          '<div class="mediumInsert-embedsWire">' +
-            '<div class="mediumInsert-embedsBox">' +
-              '<input type="text" class="mediumInsert-embedsText" placeholder="type or paste url here">' +
-              '<button class="mediumInsert-embedsButton">Embed</button>' +
-            '</div>' +
-          '</div>' +
-        '</div>' +
-      '</div>' +
-    '</div>'
-  );
-
-  $.fn.mediumInsert.getAddon('embeds');
-
-  equal($('.mediumInsert-embedsPlaceholder', $el).length, 1, 'embed placeholder removed');
-  $(document).one('click', '.mediumInsert-embedsButton', function () {
-    equal($('.mediumInsert-embedsPlaceholder', $el).length, 0, 'embed placeholder removed');
-    ok($('.mediumInsert-placeholder', $el).length > 0, 'embed social service tags');
-    start();
-  });
-
-  $('.mediumInsert-embedsText', $el).val('https://www.youtube.com/watch?v=rlNmYgNdOkM');
-  $('.mediumInsert-embedsButton', $el).click();
-});
-
-asyncTest('embed "Instagram" url', function () {
-  var $el = $('#qunit-fixture').html('<div class="mediumInsert" contenteditable="false">' +
-    '<div class="mediumInsert-placeholder">' +
-      '<div class="mediumInsert-embedsPlaceholder">' +
-          '<i class="fa fa-code mediumInsert-embedsImage"></i>' +
-          '<div class="mediumInsert-embedsWire">' +
-            '<div class="mediumInsert-embedsBox">' +
-              '<input type="text" class="mediumInsert-embedsText" placeholder="type or paste url here">' +
-              '<button class="mediumInsert-embedsButton">Embed</button>' +
-            '</div>' +
-          '</div>' +
-        '</div>' +
-      '</div>' +
-    '</div>'
-  );
-
-  $.fn.mediumInsert.getAddon('embeds');
-
-  equal($('.mediumInsert-embedsPlaceholder', $el).length, 1, 'embed placeholder removed');
-  $(document).one('click', '.mediumInsert-embedsButton', function () {
-    equal($('.mediumInsert-embedsPlaceholder', $el).length, 0, 'embed placeholder removed');
-    ok($('.mediumInsert-placeholder', $el).length > 0, 'embed social service tags');
-    start();
-  });
-
-  $('.mediumInsert-embedsText', $el).val('http://instagram.com/p/ivS5DStzJd/');
-  $('.mediumInsert-embedsButton', $el).click();
+  var e = $.Event("keypress");
+  e.keyCode = 13;
+  $('.mediumInsert-embedsText', $el).val('https://twitter.com/phpstorm/statuses/467987788659720192').trigger(e);
+  $('.mediumInsert-embedsText', $el).val('https://www.facebook.com/kensuu/posts/10152132714538568').trigger(e);
+  $('.mediumInsert-embedsText', $el).val('http://instagram.com/p/ivS5DStzJd/').trigger(e);
+  $('.mediumInsert-embedsText', $el).val('http://vimeo.com/94923911').trigger(e);
+  $('.mediumInsert-embedsText', $el).val('https://www.youtube.com/watch?v=rlNmYgNdOkM').trigger(e);
 });
 
