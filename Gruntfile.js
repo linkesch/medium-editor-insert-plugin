cfdf223c297ed40aa56715335200c454d4a5f7c6
Pavel Linkesch <pavel.linkesch@gmail.com>
2014-08-06 13:35:05 +0200

    add non-minified files to dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,14 +5,14 @@ module.exports = function(grunt) {
     banner: '/*! \n * <%= pkg.name %> v<%= pkg.version %> - <%= pkg.description %>\n *\n * <%= pkg.homepage %>\n * \n * Copyright (c) 2014 <%= pkg.author.name %> (<%= pkg.author.url %>)\n * Released under the <%= pkg.license %> license\n */\n\n',
 
     uglify: {
-      build: {
+      dist: {
         options: {
           banner: '<%= banner %>'
         },
-        src: ['src/js/medium-editor-insert-plugin.js', 'src/js/medium-editor-insert-images.js', 'src/js/medium-editor-insert-maps.js', 'src/js/medium-editor-insert-embeds.js', 'src/js/medium-editor-insert-tables.js'],
+        src: ['src/js/medium-editor-insert-plugin.js', 'src/js/*.js'],
         dest: 'dist/js/<%= pkg.name %>.all.min.js'
       },
-      build2: {
+      addons: {
         options: {
           banner: '<%= banner %>'
         },
@@ -22,6 +22,28 @@ module.exports = function(grunt) {
           src: '**/*.js',
           dest: 'dist/js/addons',
           ext: '.min.js'
+        }]
+      }
+    },
+
+    concat: {
+      dist: {
+        options: {
+          banner: '<%= banner %>'
+        },
+        src: ['src/js/medium-editor-insert-plugin.js', 'src/js/*.js'],
+        dest: 'dist/js/<%= pkg.name %>.all.js'
+      },
+      addons: {
+        options: {
+          banner: '<%= banner %>'
+        },
+        files: [{
+          expand: true,
+          cwd: 'src/js',
+          src: '**/*.js',
+          dest: 'dist/js/addons',
+          ext: '.js'
         }]
       }
     },
@@ -57,11 +79,30 @@ module.exports = function(grunt) {
 
     csso: {
       dist: {
+        options: {
+          banner: '<%= banner %>'
+        },
         expand: true,
         cwd: 'dist/css/',
         src: ['*.css', '!*.min.css'],
         dest: 'dist/css/',
         ext: '.min.css'
+      }
+    },
+
+    usebanner: {
+      dist: {
+        options: {
+          banner: '<%= banner %>',
+          linebreak: false
+        },
+        files: [{
+          expand: true,
+          cwd: 'dist/css/',
+          src: ['*.css', '!*.min.css'],
+          dest: 'dist/css/',
+          ext: '.css'
+        }]
       }
     },
 
@@ -88,13 +129,15 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-qunit');
   grunt.loadNpmTasks('grunt-contrib-compass');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-autoprefixer');
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-csso');
+  grunt.loadNpmTasks('grunt-banner');
 
   grunt.registerTask('test', ['jshint', 'qunit']);
-  grunt.registerTask('js', ['test', 'uglify']);
-  grunt.registerTask('css', ['sass', 'autoprefixer', 'csso']);
+  grunt.registerTask('js', ['test', 'uglify', 'concat']);
+  grunt.registerTask('css', ['sass', 'autoprefixer', 'csso', 'usebanner']);
   grunt.registerTask('default', ['js', 'css']);
 
 };
