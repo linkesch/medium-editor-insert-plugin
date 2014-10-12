cb04260f0145061921294fe9fb5a7218ad5b0787
Pavel Linkesch <pavel.linkesch@gmail.com>
2014-10-12 16:17:38 +0200

    add "start" to jshintrc, add jshintrc to gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,108 +1,111 @@
 module.exports = function(grunt) {
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    banner: '/*! \n * <%= pkg.name %> v<%= pkg.version %> - <%= pkg.description %>\n *\n * <%= pkg.homepage %>\n * \n * Copyright (c) 2014 <%= pkg.author.name %> (<%= pkg.author.url %>)\n * Released under the <%= pkg.license %> license\n */\n\n',
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        banner: '/*! \n * <%= pkg.name %> v<%= pkg.version %> - <%= pkg.description %>\n *\n * <%= pkg.homepage %>\n * \n * Copyright (c) 2014 <%= pkg.author.name %> (<%= pkg.author.url %>)\n * Released under the <%= pkg.license %> license\n */\n\n',
 
-    jshint: {
-      files: ['src/js/*.js', '!src/js/templates.js', 'test/*.js']
-    },
-
-    qunit: {
-      unit: 'test.html'
-    },
-
-    sass: {
-      dist: {
-        files: [{
-          expand: true,
-          cwd: 'src/sass/',
-          src: ['*.scss'],
-          dest: 'dist/css/',
-          ext: '.css'
-        }]
-      }
-    },
-
-    autoprefixer: {
-      dist: {
-        src: 'dist/css/*.css'
-      }
-    },
-
-    csso: {
-      dist: {
-        options: {
-          banner: '<%= banner %>'
+        jshint: {
+            options: {
+                jshintrc: true
+            },
+            files: ['src/js/*.js', '!src/js/templates.js', 'test/*.js']
         },
-        expand: true,
-        cwd: 'dist/css/',
-        src: ['*.css', '!*.min.css'],
-        dest: 'dist/css/',
-        ext: '.min.css'
-      }
-    },
 
-    usebanner: {
-      dist: {
-        options: {
-          banner: '<%= banner %>',
-          linebreak: false
+        qunit: {
+            unit: 'test.html'
         },
-        files: [{
-          expand: true,
-          cwd: 'dist/css/',
-          src: ['*.css', '!*.min.css'],
-          dest: 'dist/css/',
-          ext: '.css'
-        }]
-      }
-    },
 
-    watch: {
-      styles: {
-        files: 'src/sass/**/*.scss',
-        tasks: ['css'],
-        options: {
-          debounceDelay: 250
-        }
-      },
-      templates: {
-        files: 'src/js/templates/**/*.hbs',
-        tasks: ['handlebars'],
-        options: {
-          debounceDelay: 250
-        }
-      }
-    },
-
-    handlebars: {
-      compile: {
-        options: {
-          namespace: 'MediumInsert.Templates'
+        sass: {
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: 'src/sass/',
+                    src: ['*.scss'],
+                    dest: 'dist/css/',
+                    ext: '.css'
+                }]
+            }
         },
-        files: {
-          "src/js/templates.js": "src/js/templates/*.hbs"
+
+        autoprefixer: {
+            dist: {
+                src: 'dist/css/*.css'
+            }
+        },
+
+        csso: {
+            dist: {
+                options: {
+                    banner: '<%= banner %>'
+                },
+                expand: true,
+                cwd: 'dist/css/',
+                src: ['*.css', '!*.min.css'],
+                dest: 'dist/css/',
+                ext: '.min.css'
+            }
+        },
+
+        usebanner: {
+            dist: {
+                options: {
+                    banner: '<%= banner %>',
+                    linebreak: false
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'dist/css/',
+                    src: ['*.css', '!*.min.css'],
+                    dest: 'dist/css/',
+                    ext: '.css'
+                }]
+            }
+        },
+
+        watch: {
+            styles: {
+                files: 'src/sass/**/*.scss',
+                tasks: ['css'],
+                options: {
+                    debounceDelay: 250
+                }
+            },
+            templates: {
+                files: 'src/js/templates/**/*.hbs',
+                tasks: ['handlebars'],
+                options: {
+                    debounceDelay: 250
+                }
+            }
+        },
+
+        handlebars: {
+            compile: {
+                options: {
+                    namespace: 'MediumInsert.Templates'
+                },
+                files: {
+                    "src/js/templates.js": "src/js/templates/*.hbs"
+                }
+            }
         }
-      }
-    },
-  });
+    });
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
-  grunt.loadNpmTasks('grunt-contrib-compass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-autoprefixer');
-  grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks('grunt-csso');
-  grunt.loadNpmTasks('grunt-banner');
-  grunt.loadNpmTasks('grunt-contrib-handlebars');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-csso');
+    grunt.loadNpmTasks('grunt-banner');
+    grunt.loadNpmTasks('grunt-contrib-handlebars');
 
-  grunt.registerTask('test', ['jshint', 'qunit']);
-  grunt.registerTask('js', ['test', 'handlebars']);
-  grunt.registerTask('css', ['sass', 'autoprefixer', 'csso', 'usebanner']);
-  grunt.registerTask('default', ['js', 'css']);
+    grunt.registerTask('test', ['jshint', 'qunit']);
+    grunt.registerTask('js', ['test', 'handlebars']);
+    grunt.registerTask('css', ['sass', 'autoprefixer', 'csso', 'usebanner']);
+    grunt.registerTask('default', ['js', 'css']);
 
 };
