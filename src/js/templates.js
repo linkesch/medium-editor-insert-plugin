27655d1ca2d929258c94fbbcde6aa51d0ff3b773
Pavel Linkesch <pavel.linkesch@gmail.com>
2014-11-04 15:18:07 +0100

    disable image resizing in firefox

--- src/js/templates.js
+++ src/js/templates.js
@@ -77,7 +77,7 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
   var buffer = "", stack1, helper, functionType="function", escapeExpression=this.escapeExpression;
 
 
-  buffer += "<figure><img src=\"";
+  buffer += "<figure contenteditable=\"false\"><img src=\"";
   if (helper = helpers.img) { stack1 = helper.call(depth0, {hash:{},data:data}); }
   else { helper = (depth0 && depth0.img); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
   buffer += escapeExpression(stack1)
