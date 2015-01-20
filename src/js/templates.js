c74809a4fe1e14ae7d6b0779c8bc7885c9968dc1
Pavel Linkesch <pavel.linkesch@gmail.com>
2015-01-20 13:46:14 +0100

    fix embeds placeholder class

--- src/js/templates.js
+++ src/js/templates.js
@@ -40,7 +40,7 @@ this["MediumInsert"]["Templates"]["src/js/templates/core-empty-line.hbs"] = Hand
 
 this["MediumInsert"]["Templates"]["src/js/templates/embeds-placeholder.hbs"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
   var helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
-  return "<div class=\"medium-insert-columns-placeholder\" contenteditable=\"false\">"
+  return "<div class=\"medium-insert-embeds-placeholder\" contenteditable=\"false\">"
     + escapeExpression(((helper = (helper = helpers.placeholder || (depth0 != null ? depth0.placeholder : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"placeholder","hash":{},"data":data}) : helper)))
     + "</div>";
 },"useData":true});
