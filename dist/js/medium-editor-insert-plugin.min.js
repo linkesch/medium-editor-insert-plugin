02d978ce92f6bf3aef6342ec41a18c42be2256dc
Pavel Linkesch <pavel.linkesch@gmail.com>
2015-02-17 14:38:15 +0100

    version bump to 1.0.3

--- dist/js/medium-editor-insert-plugin.min.js
+++ dist/js/medium-editor-insert-plugin.min.js
@@ -1,5 +1,5 @@
 /*! 
- * medium-editor-insert-plugin v1.0.2 - jQuery insert plugin for MediumEditor
+ * medium-editor-insert-plugin v1.0.3 - jQuery insert plugin for MediumEditor
  *
  * https://github.com/orthes/medium-editor-insert-plugin
  * 
