4a825dd6e95c226feb9a9054148586ab2b6e4da0
Jeremy <jeremy.benoist@gmail.com>
2015-04-30 14:18:50 +0200

    Remove commented test & manual prefixes in css

--- test/core.js
+++ test/core.js
@@ -154,8 +154,6 @@ test('toggling addons buttons after clicking on show button', function () {
 
     this.$el.find('.medium-insert-buttons-show').click();
 
-    // hidden the buttons will trigger fadeToggle which will fade the element. So the display will be block until the fade is finish.
-    // equal(this.$el.find('.medium-insert-buttons-addons').css('display'), 'none', 'addons are hidden');
     ok(!this.$el.find('.medium-insert-buttons-show').hasClass('medium-insert-buttons-rotate'), '.medium-insert-buttons-rotate class removed');
 });
 
