a0fa45f759f1c524e5442c68926b7fbec823baae
aggied <sploople@gmail.com>
2015-11-07 21:16:01 -0500

    remove console.log

--- src/js/embeds.js
+++ src/js/embeds.js
@@ -297,10 +297,11 @@
             },
             success: function(data) {
                 var html = data && data.html;
-		 if (that.options.storeMeta){
-                    console.log('storig meta');
+
+		if (that.options.storeMeta){
                     html+='<div class="medium-insert-meta"><script type="text/json">'+JSON.stringify(data)+'</script></div>';                    
                 }
+
                 if (data && !data.html && data.type === 'photo' && data.url) {
                     html = '<img src="' + data.url + '" alt="">';
                 }
