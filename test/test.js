3dd2373d067e064557aa7112a18ec56bf8fe94b1
Erik Thorne <erik.j.thorne@gmail.com>
2015-11-22 13:11:03 -0800

    cleanup

--- test/test.js
+++ test/test.js
@@ -2,17 +2,16 @@ $(document).ready(function(){
 	var medEditor = new MediumEditor('.editable');
 	$('.editable').mediumInsert({
 		editor: medEditor,
-		enabled: false,
+		enabled: true,
 		buttonLabels: 'fontawesome'
 	});
 
 	$('#button').click(function()
 	{
 		var allContents = medEditor.serialize();
-		var elContent = allContents["element-0"].value;
+		var elContent = allContents['element-0'].value;
 
 		console.log(allContents);
 		$('#elContents').text(elContent);
 	});
 });
-
