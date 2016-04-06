dad1ea0e0d2f3c57238729720a78c886d95a20a4
Pavel Linkesch <pavel.linkesch@gmail.com>
2016-04-06 11:10:52 +0200

    add umd wrapper and prepare package.json for publishin on npm

---
+++ src/wrappers/start.js
@@ -0,0 +1,20 @@
+(function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(['jquery'], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        module.exports = function( root, jQuery ) {
+            if ( jQuery === undefined ) {
+                if ( typeof window !== 'undefined' ) {
+                    jQuery = require('jquery');
+                }
+                else {
+                    jQuery = require('jquery')(root);
+                }
+            }
+            factory(jQuery);
+            return jQuery;
+        };
+    } else {
+        factory(jQuery);
+    }
+}(function ($) {
