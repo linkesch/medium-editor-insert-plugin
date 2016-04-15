359a818c3d6a0eaf5dd50a50fb14d447896b37cd
Serhiy Moskovchuk <serhiy.moskovchuk@objectway.it>
2016-04-15 17:15:32 +0200

    fix missing dependencies

--- src/wrappers/start.js
+++ src/wrappers/start.js
@@ -1,14 +1,18 @@
 (function (factory) {
     if (typeof define === 'function' && define.amd) {
-        define(['jquery', 'handlebars', 'medium-editor'], factory);
+        define(['jquery', 'handlebars', 'medium-editor', 'blueimp-file-upload', 'jquery-sortable'], factory);
     } else if (typeof module === 'object' && module.exports) {
         module.exports = function( root, jQuery ) {
             if ( jQuery === undefined ) {
                 if ( typeof window !== 'undefined' ) {
                     jQuery = require('jquery');
+                    Handlebars = require('handlebars');
+                    MediumEditor = require('medium-editor');
                 }
                 else {
                     jQuery = require('jquery')(root);
+                    Handlebars = require('handlebars')(root);
+                    MediumEditor = require('medium-editor')(root);
                 }
             }
             factory(jQuery, Handlebars, MediumEditor);
