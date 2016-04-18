9a122cbbc3bf89ab5eb0efcc14579ec3ad8e6d30
sdqwerty <serhiy.mos@mail.ru>
2016-04-18 11:03:13 +0200

    Fix AMD Dependency (#314)
    * Fix AMD Dependency, add handlebars and medium editor to the factory arguments
    
    * fix missing dependencies
    
    * restore prev dist and templates.js
    
    * dist
    
    * restore dist

--- src/wrappers/start.js
+++ src/wrappers/start.js
@@ -1,20 +1,24 @@
 (function (factory) {
     if (typeof define === 'function' && define.amd) {
-        define(['jquery'], factory);
+        define(['jquery', 'handlebars', 'medium-editor', 'blueimp-file-upload', 'jquery-sortable'], factory);
     } else if (typeof module === 'object' && module.exports) {
         module.exports = function( root, jQuery ) {
             if ( jQuery === undefined ) {
                 if ( typeof window !== 'undefined' ) {
                     jQuery = require('jquery');
+                    Handlebars = require('handlebars');
+                    MediumEditor = require('medium-editor');
                 }
                 else {
                     jQuery = require('jquery')(root);
+                    Handlebars = require('handlebars')(root);
+                    MediumEditor = require('medium-editor')(root);
                 }
             }
-            factory(jQuery);
+            factory(jQuery, Handlebars, MediumEditor);
             return jQuery;
         };
     } else {
-        factory(jQuery);
+        factory(jQuery, Handlebars, MediumEditor);
     }
-}(function ($) {
+}(function ($, Handlebars, MediumEditor) {
