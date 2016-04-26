c39da02d59b161548befc7f0b39b20ea88876d32
Pavel Linkesch <pavel.linkesch@gmail.com>
2016-04-26 10:41:02 +0200

    v2.3.2

--- dist/js/medium-editor-insert-plugin.js
+++ dist/js/medium-editor-insert-plugin.js
@@ -1,5 +1,5 @@
 /*! 
- * medium-editor-insert-plugin v2.3.1 - jQuery insert plugin for MediumEditor
+ * medium-editor-insert-plugin v2.3.2 - jQuery insert plugin for MediumEditor
  *
  * http://linkesch.com/medium-editor-insert-plugin
  * 
@@ -9,21 +9,23 @@
 
 (function (factory) {
     if (typeof define === 'function' && define.amd) {
-        define(['jquery', 'handlebars', 'medium-editor', 'blueimp-file-upload', 'jquery-sortable'], factory);
+        define(['jquery', 'handlebars/runtime', 'medium-editor', 'blueimp-file-upload', 'jquery-sortable'], factory);
     } else if (typeof module === 'object' && module.exports) {
-        module.exports = function( root, jQuery ) {
-            if ( jQuery === undefined ) {
-                if ( typeof window !== 'undefined' ) {
-                    jQuery = require('jquery');
-                    Handlebars = require('handlebars');
-                    MediumEditor = require('medium-editor');
-                }
-                else {
-                    jQuery = require('jquery')(root);
-                    Handlebars = require('handlebars')(root);
-                    MediumEditor = require('medium-editor')(root);
-                }
+        module.exports = function (jQuery) {
+            if (typeof window === 'undefined') {
+                throw new Error("medium-editor-insert-plugin runs only in a browser.")
             }
+
+            if (jQuery === undefined) {
+                jQuery = require('jquery');
+            }
+            window.jQuery = jQuery;
+
+            Handlebars = require('handlebars/runtime');
+            MediumEditor = require('medium-editor');
+            require('jquery-sortable');
+            require('blueimp-file-upload');
+
             factory(jQuery, Handlebars, MediumEditor);
             return jQuery;
         };
