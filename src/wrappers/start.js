15e20417b070ac31ebe1a3f8af4ecdd9a4031de9
Pavel Linkesch <pavel.linkesch@gmail.com>
2016-04-26 10:38:52 +0200

    fix umd (#320)

--- src/wrappers/start.js
+++ src/wrappers/start.js
@@ -1,20 +1,22 @@
 (function (factory) {
     if (typeof define === 'function' && define.amd) {
-        define(['jquery', 'handlebars', 'medium-editor', 'blueimp-file-upload', 'jquery-sortable'], factory);
+        define(['jquery', 'handlebars/runtime', 'medium-editor', 'blueimp-file-upload', 'jquery-sortable'], factory);
     } else if (typeof module === 'object' && module.exports) {
-        module.exports = function( root, jQuery ) {
-            if ( jQuery === undefined ) {
-                if ( typeof window !== 'undefined' ) {
-                    jQuery = require('jquery');
-                    Handlebars = require('handlebars');
-                    MediumEditor = require('medium-editor');
-                }
-                else {
-                    jQuery = require('jquery')(root);
-                    Handlebars = require('handlebars')(root);
-                    MediumEditor = require('medium-editor')(root);
-                }
+        module.exports = function (jQuery) {
+            if (typeof window === 'undefined') {
+                throw new Error("medium-editor-insert-plugin runs only in a browser.")
             }
+
+            if (jQuery === undefined) {
+                jQuery = require('jquery');
+            }
+            window.jQuery = jQuery;
+
+            Handlebars = require('handlebars/runtime');
+            MediumEditor = require('medium-editor');
+            require('jquery-sortable');
+            require('blueimp-file-upload');
+
             factory(jQuery, Handlebars, MediumEditor);
             return jQuery;
         };
