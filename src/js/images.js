b071b0eb218e40290028c36cafbb05de6411cbda
Artem Shevtsov <shevtsov.artem@gmail.com>
2018-02-23 22:26:20 +0300

    Getting relative path for uploaded image, instead of absolute (#455)
    * Getting relative path for uploaded image, instead of absolute
    
    * Getting relative path for uploaded image, instead of absolute. BUILD
    
    * build again, node updated
    
    * Revert "build again, node updated"
    
    This reverts commit b2b24041a665df8d49b82e7877e073be3e62e58c.
    
    * Revert "Getting relative path for uploaded image, instead of absolute. BUILD"
    
    This reverts commit fc088de84a0252bceed7c050beac00997742c161.

--- src/js/images.js
+++ src/js/images.js
@@ -386,7 +386,7 @@
         if (this.options.preview && data.context) {
             domImage = this.getDOMImage();
             domImage.onload = function () {
-                data.context.find('img').attr('src', domImage.src);
+                data.context.find('img').attr('src', domImage.getAttribute('src'));
 
                 if (this.options.uploadCompleted) {
                     this.options.uploadCompleted(data.context, data);
