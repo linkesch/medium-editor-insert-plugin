c34c889a085dcdc4df77c79b4f502b7816b34af8
sa-inu <uovo.aureo.000@gmail.com>
2018-09-17 15:57:35 +0900

    Enable to select image on mobile device

--- src/js/images.js
+++ src/js/images.js
@@ -145,7 +145,7 @@
             .on('click', '.medium-insert-images-toolbar2 .medium-editor-action', $.proxy(this, 'toolbar2Action'));
 
         this.$el
-            .on('click', '.medium-insert-images img', $.proxy(this, 'selectImage'));
+            .on('click touchend', '.medium-insert-images img', $.proxy(this, 'selectImage'));
 
         $(window)
             .on('resize', $.proxy(this, 'autoRepositionToolbars'));
