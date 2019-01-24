e11e0130f12321ea5c832a467174025734388266
Pavel Linkesch <pavel.linkesch@gmail.com>
2019-01-24 12:29:49 +0100

    chore: update dependencies (#488)

--- Gruntfile.js
+++ Gruntfile.js
@@ -77,6 +77,9 @@ module.exports = function (grunt) {
         },
 
         sass: {
+            options: {
+                implementation: require('node-sass')
+            },
             dist: {
                 files: [{
                     expand: true,
